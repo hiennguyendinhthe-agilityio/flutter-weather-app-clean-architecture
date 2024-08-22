315f7c8291f7397e071952a58b9c0407ddc3c274
hien.nguyendinhthe <hien.nguyendinhthe@asnet.com.vn>
2024-08-22 17:09:36 +0700

    Training app testing

--- apps/app_testing/lib/main.dart
+++ apps/app_testing/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:app_testing/about.dart';
+import 'package:app_testing/counter.dart';
 import 'package:app_testing/home.dart';
+import 'package:app_testing/models/counter_porvider.dart';
 import 'package:app_testing/models/ui.dart';
 import 'package:app_testing/settings.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +22,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (_) => UI(),
         ),
+        ChangeNotifierProvider(create: (_) => CounterPorvider())
       ],
       child: MaterialApp(
         initialRoute: '/',
@@ -27,6 +30,7 @@ class MyApp extends StatelessWidget {
           '/': (context) => const Home(),
           '/about': (context) => const About(),
           '/settings': (context) => const Settings(),
+          '/counter': (context) => const Counter(),
         },
       ),
     );
