3216a080553e0d4a914a62830e308984e608cff9
Hien Dinh The Nguyen <hien.nguyendinhthe@asnet.com.vn>
2024-09-12 17:35:02 +0700

    Implement bloc product

---
+++ apps/flutter_practice/bazar_books_app/lib/features/home/widgets/product/products.dart
@@ -0,0 +1,55 @@
+import 'package:bazar_books_app/features/home/bloc/product_bloc.dart';
+import 'package:bazar_books_design/bazar_books_design.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../detail_menu/detail_menu_page.dart';
+
+class Products extends StatelessWidget {
+  const Products({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 23),
+      child: SizedBox(
+        height: 180,
+        child: BlocBuilder<ProductBloc, GetProductsState>(
+          builder: (context, state) {
+            if (state is GetProductsStateLoading) {
+              return const ShimmerListItem();
+            } else if (state is GetProductsStateLoaded) {
+              return ListView.builder(
+                itemCount: state.products.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  final product = state.products[index];
+                  return BookCard(
+                    onTap: () {
+                      BazUiBottomSheet.showModal(
+                        context,
+                        child: const DetailMenuPage(),
+                      );
+                    },
+                    title: product.title,
+                    price: product.price,
+                    imageUrl: product.imageUrl,
+                  );
+                },
+              );
+            } else if (state is GetProductsStateError) {
+              return Text(
+                textAlign: TextAlign.center,
+                'Error: ${state.message}',
+              );
+            } else {
+              return const SizedBox.shrink();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
