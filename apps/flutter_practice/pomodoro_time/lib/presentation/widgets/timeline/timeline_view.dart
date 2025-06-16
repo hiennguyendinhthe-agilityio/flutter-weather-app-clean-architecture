bda79eb5f7486741d0348f1549a30fd57707cd5c
hien.nguyendinhthe <hien.nguyendinhthe@asnet.com.vn>
2025-06-16 17:39:29 +0700

    Add configurable eventSpacing to TimelineView for horizontal gaps between columns

--- apps/flutter_practice/pomodoro_time/lib/presentation/widgets/timeline/timeline_view.dart
+++ apps/flutter_practice/pomodoro_time/lib/presentation/widgets/timeline/timeline_view.dart
@@ -4,20 +4,38 @@ import 'column_allocator.dart';
 import 'time_axis_painter.dart';
 import 'timeline_event.dart';
 
+// Define a callback function type for building timeline items
 typedef TimelineItemBuilder<T> = Widget Function(
   BuildContext context,
   T data,
   bool isCompact,
 );
 
+// A widget for displaying a timeline of events
 class TimelineView<T> extends StatelessWidget {
+  // The list of events to display
   final List<TimelineEvent<T>> events;
+
+  // A callback function for building individual timeline items
   final TimelineItemBuilder<T> itemBuilder;
+
+  // The height of each hour in the timeline
   final double hourHeight;
+
+  // The minimum duration of an event
   final Duration minEventDuration;
+
+  // An optional scroll controller for the timeline
   final ScrollController? scrollController;
+
+  // The style for time labels
   final TextStyle timeLabelStyle;
+
+  // The padding for the timeline
   final EdgeInsets padding;
+
+  // The spacing between events
+  final double eventSpacing;
 
   const TimelineView({
     required this.events,
@@ -27,25 +45,39 @@ class TimelineView<T> extends StatelessWidget {
     this.scrollController,
     this.timeLabelStyle = const TextStyle(color: Colors.grey),
     this.padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+    this.eventSpacing = 8,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Sort the events by start time
     final sorted = List<TimelineEvent<T>>.from(events)
       ..sort((a, b) => a.start.compareTo(b.start));
+
+    // Allocate events to columns
     final columns = ColumnAllocator<TimelineEvent<T>>().allocate(
       sorted,
       (a, b) => a.start.isBefore(b.end) && b.start.isBefore(a.end),
     );
 
+    // Calculate the total height of the timeline
     final totalHeight = 24 * hourHeight;
+
+    // Define the width of time labels
     const timeLabelWidth = 68.0;
+
+    // Calculate the available width for events
     final availableWidth =
         MediaQuery.of(context).size.width - timeLabelWidth - padding.horizontal;
-    final colCount = columns.length;
-    final colWidth = availableWidth / (colCount > 0 ? colCount : 1);
 
+    // Calculate the number of columns and the width of each column
+    final colCount = columns.length;
+    final totalGaps = eventSpacing * (colCount - 1);
+    final colWidth =
+        (availableWidth - totalGaps) / (colCount > 0 ? colCount : 1);
+
+    // Return a scrollable timeline
     return SingleChildScrollView(
       controller: scrollController,
       padding: padding,
@@ -53,6 +85,7 @@ class TimelineView<T> extends StatelessWidget {
         height: totalHeight,
         child: Stack(
           children: [
+            // Paint the time axis
             CustomPaint(
               size: Size(double.infinity, totalHeight),
               painter: TimeAxisPainter(
@@ -60,6 +93,7 @@ class TimelineView<T> extends StatelessWidget {
                 labelStyle: timeLabelStyle,
               ),
             ),
+            // Build events for each column
             for (var colIdx = 0; colIdx < colCount; colIdx++)
               for (final evt in columns[colIdx])
                 _buildEvent(context, evt, colIdx, colWidth, timeLabelWidth),
@@ -69,6 +103,7 @@ class TimelineView<T> extends StatelessWidget {
     );
   }
 
+  // Build an individual event
   Widget _buildEvent(
     BuildContext ctx,
     TimelineEvent<T> evt,
@@ -76,18 +111,20 @@ class TimelineView<T> extends StatelessWidget {
     double colWidth,
     double timeLabelWidth,
   ) {
+    // Calculate the start and end times of the event
     final startMin = evt.start.hour * 60 + evt.start.minute;
     final endMin = evt.end.hour * 60 + evt.end.minute;
+
+    // Calculate the position and size of the event
     final top = startMin * (hourHeight / 60);
     final rawHeight = (endMin - startMin) * (hourHeight / 60);
-
     final minH = minEventDuration.inMinutes * (hourHeight / 60);
     final height = rawHeight < minH ? minH : rawHeight;
-
-    final left = timeLabelWidth + colIdx * colWidth;
+    final left = timeLabelWidth + colIdx * (colWidth + eventSpacing);
     final width = colWidth;
     final isCompact = rawHeight < minH;
 
+    // Return a positioned event
     return Positioned(
       top: top,
       left: left,
