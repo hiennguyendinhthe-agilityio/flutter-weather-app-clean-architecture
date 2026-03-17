c3eb15dcd208a9f14636b427cda8be107ab6da16
hien.nguyendinhthe <hien.nguyendinhthe@asnet.com.vn>
2026-03-17 17:31:59 +0700

    Practical Mini Implementation Animation

---
+++ flutter_application_2/lib/explicit_animation/loading_sniper.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class LoadingSniper extends StatefulWidget {
+  const LoadingSniper({super.key});
+
+  @override
+  State<LoadingSniper> createState() => _LoadingSniperState();
+}
+
+class _LoadingSniperState extends State<LoadingSniper>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.rotate(
+              angle: _controller.value * 2 * pi,
+              child: child,
+            );
+          },
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              border: Border.all(width: 10, color: Colors.purple),
+              color: Colors.purple,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Align(
+              alignment: Alignment.center,
+              child: Icon(Icons.refresh, color: Colors.white, size: 50),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
